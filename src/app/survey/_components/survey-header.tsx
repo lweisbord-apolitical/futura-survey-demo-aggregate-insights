fd4ae69fa090d7b3aaa9b377a7c58110fc0624a0
Lowell weisbord <lowellweisbord@gmail.com>
2026-01-28 19:58:54 +0000

    Reduce header padding further

--- src/app/survey/_components/survey-header.tsx
+++ src/app/survey/_components/survey-header.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 export function SurveyHeader() {
   return (
     <header className="border-b border-neutral-100">
-      <div className="px-6 sm:px-8 py-2.5">
+      <div className="px-6 sm:px-8 py-1.5">
         {/* Logo */}
         <Image
           src="/apolitical-logo.png"
