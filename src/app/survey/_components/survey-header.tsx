22bb48f7c99d4e953c13e3053f8b58a53c007626
Lowell weisbord <lowellweisbord@gmail.com>
2026-01-28 19:57:36 +0000

    Reduce header padding, increase logo size

--- src/app/survey/_components/survey-header.tsx
+++ src/app/survey/_components/survey-header.tsx
@@ -5,13 +5,13 @@ import Image from "next/image";
 export function SurveyHeader() {
   return (
     <header className="border-b border-neutral-100">
-      <div className="px-6 sm:px-8 py-4">
+      <div className="px-6 sm:px-8 py-2.5">
         {/* Logo */}
         <Image
           src="/apolitical-logo.png"
           alt="Apolitical"
-          width={180}
-          height={42}
+          width={200}
+          height={47}
           priority
         />
       </div>
