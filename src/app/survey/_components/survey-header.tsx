65ea312024656cea0bf6d3acbb509708168c2755
Lowell weisbord <lowellweisbord@gmail.com>
2026-01-28 19:59:17 +0000

    Remove header border line

--- src/app/survey/_components/survey-header.tsx
+++ src/app/survey/_components/survey-header.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 export function SurveyHeader() {
   return (
-    <header className="border-b border-neutral-100">
+    <header>
       <div className="px-6 sm:px-8 py-1.5">
         {/* Logo */}
         <Image
